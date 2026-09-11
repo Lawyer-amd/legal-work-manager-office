db1dd208a7b89ed40cbfaf89af23cc7e094b45e3
Lawyer-amd <kuod21@gmail.com>
2026-09-12 02:38:35 +0300

    chore: point app to automatic sync deployment

--- src/cloudSync.ts
+++ src/cloudSync.ts
@@ -1,6 +1,6 @@
 import type { Database } from './dataStore'
 
-export const defaultCloudEndpoint = 'https://script.google.com/macros/s/AKfycbx5LhdN9WykSLjP0OHBsRQe_uVhFpw2e6mi_fT8J25GtAiz8exTurUfsozo5_a7kofAkA/exec'
+export const defaultCloudEndpoint = 'https://script.google.com/macros/s/AKfycbw_zsFooJ26WodKcO6oOnSbZc13iseJGwfehHz60D71kmp7NTYpYZsYAUY-K16e3IXubw/exec'
 const requestTimeoutMs = 15_000
 const apiEndpoint = (endpoint: string) => `${endpoint}${endpoint.includes('?') ? '&' : '?'}api=1`
 
